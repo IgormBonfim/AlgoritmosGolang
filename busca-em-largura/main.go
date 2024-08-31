3236afd18feb56a05b4cad7aefd16ea2c1f03574
Igor Bonfim <igormeirabonfim@gmail.com>
2024-08-31 18:49:19 -0300

    Separa criação de network em function

--- busca-em-largura/main.go
+++ busca-em-largura/main.go
@@ -7,23 +7,11 @@ import (
 
 func main() {
 
-	diogo := desenvolvedor.New("Diogo", make([]desenvolvedor.Desenvolvedor, 0), "Golang")
-	fernando := desenvolvedor.New("Fernando", make([]desenvolvedor.Desenvolvedor, 0), "PHP")
-	emanuel := desenvolvedor.New("Emanuel", []desenvolvedor.Desenvolvedor{fernando}, "JavaScript")
-	andre := desenvolvedor.New("Andre", []desenvolvedor.Desenvolvedor{fernando, emanuel}, "JavaScript")
-	ricardo := desenvolvedor.New("Ricardo", []desenvolvedor.Desenvolvedor{fernando, andre, emanuel, diogo}, "C#")
-	ana := desenvolvedor.New("Ana", []desenvolvedor.Desenvolvedor{fernando, ricardo, andre, emanuel}, "Java")
-	mauricio := desenvolvedor.New("Mauricio", []desenvolvedor.Desenvolvedor{fernando, ana, ricardo, andre, emanuel}, "C#")
-
-	diego := desenvolvedor.New("Diego", []desenvolvedor.Desenvolvedor{emanuel, fernando}, "JavaScript")
-	pablo := desenvolvedor.New("Pablo", []desenvolvedor.Desenvolvedor{andre, ricardo, fernando}, "Java")
-	matheus := desenvolvedor.New("Matheus", []desenvolvedor.Desenvolvedor{pablo, ricardo, ana, mauricio}, "TypeScript")
-
-	igor := desenvolvedor.New("Igor Bonfim", []desenvolvedor.Desenvolvedor{pablo, matheus, diego}, "C#")
+	igor := createDevsNetwork()
 
 	fmt.Printf("Buscando um Dev Golang\n")
 
-	resultado, err := buscaEmLargura(&igor)
+	resultado, err := buscaEmLargura(igor)
 
 	if err != nil {
 		fmt.Println(err.Error())
@@ -44,7 +32,7 @@ func buscaEmLargura(dev *desenvolvedor.Desenvolvedor) (*desenvolvedor.Desenvolve
 		fmt.Printf("Verificando se %s programa em Golang \n", queue[0].Nome)
 
 		if verificados[queue[0].Nome] {
-			fmt.Printf("O Dev %s já foi verificado \n", queue[0].Nome)
+			fmt.Printf("%s já foi verificado \n", queue[0].Nome)
 			queue = queue[1:]
 			continue
 		}
@@ -60,4 +48,22 @@ func buscaEmLargura(dev *desenvolvedor.Desenvolvedor) (*desenvolvedor.Desenvolve
 	}
 
 	return nil, fmt.Errorf("nenhum Dev Golang encontrado")
+}
+
+func createDevsNetwork() *desenvolvedor.Desenvolvedor {
+	diogo := desenvolvedor.New("Diogo", make([]desenvolvedor.Desenvolvedor, 0), "Golang")
+	fernando := desenvolvedor.New("Fernando", make([]desenvolvedor.Desenvolvedor, 0), "PHP")
+	emanuel := desenvolvedor.New("Emanuel", []desenvolvedor.Desenvolvedor{fernando}, "JavaScript")
+	andre := desenvolvedor.New("Andre", []desenvolvedor.Desenvolvedor{fernando, emanuel}, "JavaScript")
+	ricardo := desenvolvedor.New("Ricardo", []desenvolvedor.Desenvolvedor{fernando, andre, emanuel, diogo}, "C#")
+	ana := desenvolvedor.New("Ana", []desenvolvedor.Desenvolvedor{fernando, ricardo, andre, emanuel}, "Java")
+	mauricio := desenvolvedor.New("Mauricio", []desenvolvedor.Desenvolvedor{fernando, ana, ricardo, andre, emanuel}, "C#")
+
+	diego := desenvolvedor.New("Diego", []desenvolvedor.Desenvolvedor{emanuel, fernando}, "JavaScript")
+	pablo := desenvolvedor.New("Pablo", []desenvolvedor.Desenvolvedor{andre, ricardo, fernando}, "Java")
+	matheus := desenvolvedor.New("Matheus", []desenvolvedor.Desenvolvedor{pablo, ricardo, ana, mauricio}, "TypeScript")
+
+	igor := desenvolvedor.New("Igor Bonfim", []desenvolvedor.Desenvolvedor{pablo, matheus, diego}, "C#")
+
+	return &igor
 }
